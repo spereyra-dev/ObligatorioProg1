1bd061c2e49a91ae468bf6612f41c7360de41bea
Santiago Pereyra <spereyra@tiendamia.com>
2021-06-20 20:52:43 -0300

    Segunda parte obligatorio

---
+++ js/clases.js
@@ -0,0 +1,170 @@
+class Donante {
+  constructor(nombre, direccion, telefono, cantidad = 0) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.cantidad = cantidad;
+  }
+  toString() {
+    return this.nombre + " (" + this.direccion + ", " + this.telefono + ")";
+  }
+}
+
+class Donacion {
+  constructor(donante, modo, monto, comentarios = "") {
+    this.donante = donante;
+    this.modo = modo;
+    this.monto = monto;
+    this.comentarios = comentarios;
+  }
+
+  ordenarDonacionesDecre(otro) {
+    let dif = otro.monto - this.monto;
+    if (dif === 0) {
+      dif = otro.donante.localeCompare(this.donante);
+    }
+    return dif;
+  }
+
+  ordenarDonacionesCreciente(otro) {
+    let dif = this.monto - otro.monto;
+    if (dif === 0) {
+      dif = this.donante.localeCompare(otro.donante);
+    }
+    return dif;
+  }
+}
+
+class Sistema {
+  constructor() {
+    this.listaDonantes = [];
+    this.listaDonaciones = [];
+  }
+
+  agregarDonante(unElemento) {
+    this.listaDonantes.push(unElemento);
+    this.listaDonantes.sort();
+  }
+
+  darTodosDonantes() {
+    return this.listaDonantes;
+  }
+
+  existeDonante(nombre) {
+    let existe = false;
+    let nombreLower = nombre.toLowerCase();
+    for (let i = 0; i < this.listaDonantes.length && !existe; i++) {
+      if (this.listaDonantes[i].nombre.toLowerCase() == nombreLower) {
+        existe = true;
+      }
+    }
+    return existe;
+  }
+
+  agregarDonacion(unElemento) {
+    this.listaDonaciones.push(unElemento);
+  }
+
+  darDonaciones() {
+    this.listaDonaciones.sort(function (a, b) {
+      return a.ordenarDonacionesDecre(b);
+    });
+    return this.listaDonaciones;
+  }
+
+  darDonacionesCreciente() {
+    this.listaDonaciones.sort(function (a, b) {
+      return a.ordenarDonacionesCreciente(b);
+    });
+    return this.listaDonaciones;
+  }
+
+  promedioDonaciones() {
+    if (this.listaDonaciones.length > 0) {
+      let suma = 0;
+      for (let i = 0; i < this.listaDonaciones.length; i++) {
+        suma += this.listaDonaciones[i].monto;
+      }
+      return parseInt(suma / parseInt(this.listaDonaciones.length));
+    }
+    return 0;
+  }
+
+  cantidadDonaciones() {
+    return this.listaDonaciones.length;
+  }
+
+  cantidadModo(modo = "efectivo") {
+    let cantidad = 0;
+    for (let i = 0; i < this.listaDonaciones.length; i++) {
+      if (this.listaDonaciones[i].modo.toLowerCase() == modo.toLowerCase()) {
+        cantidad++;
+      }
+    }
+    return cantidad;
+  }
+  montoDonacionMayor() {
+    if (!this.darDonaciones().length > 0) {
+      return 0;
+    }
+    return this.darDonaciones()[0].monto;
+  }
+
+  montoTotalGeneral() {
+    let cantidad = 0;
+    for (let i = 0; i < this.listaDonaciones.length; i++) {
+      cantidad += this.listaDonaciones[i].monto;
+    }
+    return cantidad;
+  }
+  masVecesDono() {
+    if (this.listaDonaciones.length == 0) {
+      return "";
+    }
+    let map = {};
+    let masVeces = this.listaDonaciones[0],
+      maxCount = 1;
+    for (let i = 0; i < this.listaDonaciones.length; i++) {
+      let el = this.listaDonaciones[i];
+      if (map[el.donante] == null) {
+        map[el.donante] = 1;
+      } else {
+        map[el.donante]++;
+      }
+      if (map[el.donante] > maxCount) {
+        masVeces = el;
+        maxCount = map[el.donante];
+      }
+    }
+    let ordenable = Object.entries(map).sort((a, b) => b - a);
+    let result = false;
+    let resultado = [];
+    if (ordenable.length > 1) {
+      for (let i = 0; i < ordenable.length - 1; i++) {
+        if (ordenable[i][1] === ordenable[i + 1][1]) {
+          let posicionCortarNombreUno = ordenable[i][0].search(/\(/g);
+          let donadorUno = ordenable[i][0].substr(0, posicionCortarNombreUno);
+          let posicionCortarNombreDos = ordenable[i + 1][0].search(/\(/g);
+          let donadorDos = ordenable[i + 1][0].substr(
+            0,
+            posicionCortarNombreDos
+          );
+          if (!resultado.includes(donadorUno.trim())) {
+            resultado.push(donadorUno.trim());
+          }
+          if (!resultado.includes(donadorDos.trim())) {
+            resultado.push(donadorDos.trim());
+          }
+          result = true;
+        }
+      }
+      if (result) {
+        return resultado.toString();
+      }
+    }
+
+    let cortar = masVeces.donante.search(/\(/g);
+    let recorta = masVeces.donante.substr(0, cortar);
+    return recorta.trim();
+  }
+}
---
+++ js/funciones.js
@@ -0,0 +1,224 @@
+window.addEventListener("load", inicio);
+let sistema = new Sistema();
+
+function inicio() {
+  if (sistema.darDonaciones().length === 0) {
+    let tablaDon = document.getElementById("tableDonaciones");
+    tablaDon.innerHTML = "SIN DONACIONES";
+  }
+
+  document
+    .getElementById("buttAgregarDonante")
+    .addEventListener("click", agregarDonante);
+
+  document
+    .getElementById("buttAgregarDonacion")
+    .addEventListener("click", agregarDonacion);
+
+  document
+    .getElementById("buttAgregarDonacion")
+    .addEventListener("click", drawChart);
+
+  document
+    .getElementById("creciente")
+    .addEventListener("click", cargarDonacionesCre);
+
+  document
+    .getElementById("decreciente")
+    .addEventListener("click", cargarDonacionesDecre);
+}
+
+function agregarDonante() {
+  let formDonante = document.getElementById("formRegistroDonante");
+  if (formDonante.reportValidity()) {
+    let nombre = document.getElementById("nombre").value;
+    let direccion = document.getElementById("direccion").value;
+    let telefono = document.getElementById("telefono").value;
+    if (!sistema.existeDonante(nombre)) {
+      let donante = new Donante(nombre, direccion, telefono);
+      sistema.agregarDonante(donante);
+      formDonante.reset();
+      recargar();
+    } else {
+      alert("Ya existe un donante con ese nombre registrado en el sistema.");
+    }
+  }
+}
+
+function cargarDonantes() {
+  let comboDonante = document.getElementById("donante");
+  comboDonante.innerHTML = "";
+  let datos = sistema.darTodosDonantes();
+  for (let elemento of datos) {
+    let opcion = document.createElement("option");
+    let nodoTexto = document.createTextNode(elemento);
+    opcion.appendChild(nodoTexto);
+    comboDonante.appendChild(opcion);
+  }
+}
+
+function agregarDonacion() {
+  let formDonacion = document.getElementById("formRegistroDonacion");
+  if (formDonacion.reportValidity()) {
+    let donante = document.getElementById("donante").value;
+    let modo = document.getElementById("modo").value;
+    let monto = parseFloat(document.getElementById("monto").value);
+    let comentarios = document.getElementById("comentarios").value;
+    let donacion = new Donacion(donante, modo, monto, comentarios);
+    sistema.agregarDonacion(donacion);
+    formDonacion.reset();
+    cargarEstadisticas();
+    document.getElementById("piechart").style.display = 'block';
+    recargar();
+  }
+}
+
+function cargarDonacionesDecre() {
+  let donaciones = sistema.darDonaciones();
+  let tabla = document.getElementById("tableDonaciones");
+  tabla.innerHTML = "";
+  crearTabla(donaciones);
+}
+
+function cargarDonacionesCre() {
+  let donaciones = sistema.darDonacionesCreciente();
+  let tabla = document.getElementById("tableDonaciones");
+  tabla.innerHTML = "";
+  crearTabla(donaciones);
+}
+
+function crearTabla(donaciones) {
+  let resaltarMonto = parseFloat(document.getElementById("monto2").value);
+  let resaltarCheck = document.getElementById("filas").checked;
+  let tabla = document.getElementById("tableDonaciones");
+  tabla.innerHTML = "";
+  if (donaciones.length === 0) {
+    tabla.innerHTML = "SIN DONACIONES";
+  } else {
+    let filaDonante = tabla.insertRow();
+    let celdaDonante = document.createElement("th");
+    celdaDonante.innerHTML = "Donante";
+    filaDonante.appendChild(celdaDonante);
+    let celdaModo = document.createElement("th");
+    celdaModo.innerHTML = "Modo";
+    filaDonante.appendChild(celdaModo);
+    let celdaMonto = document.createElement("th");
+    celdaMonto.innerHTML = "Monto";
+    filaDonante.appendChild(celdaMonto);
+    let celdaComentario = document.createElement("th");
+    celdaComentario.innerHTML = "Comentarios";
+    filaDonante.appendChild(celdaComentario);
+    let titulos = document.querySelectorAll("th");
+
+    for (let i = 0; i < titulos.length; i++) {
+      if (titulos[i].textContent.trim() > "") {
+        titulos[i].classList.add("titleTable");
+      }
+    }
+
+    for (let elemento of donaciones) {
+      let fila = tabla.insertRow();
+      let celda = fila.insertCell();
+      if (resaltarCheck && elemento.monto === resaltarMonto) {
+        fila.classList.add("resaltado");
+      }
+      celda.classList.add("nombreDonantes");
+      celda.innerHTML = elemento.donante;
+      let celdaModo = fila.insertCell();
+      celdaModo.innerHTML = elemento.modo;
+      let celdaMonto = fila.insertCell();
+      celdaMonto.innerHTML = elemento.monto;
+      if (elemento.monto > 1000) {
+        celdaMonto.classList.add("mayorMil");
+      } else {
+        celdaMonto.classList.add("menorMil");
+      }
+      let celdaComentarios = fila.insertCell();
+      celdaComentarios.innerHTML = elemento.comentarios;
+    }
+  }
+}
+
+function recargar() {
+  cargarDonantes();
+  actualOrder();
+  montoMayor();
+  totalGeneral();
+}
+
+function montoMayor(){
+  let montoT = document.getElementById("montoMayor");
+  montoT.innerHTML='$ '+sistema.montoDonacionMayor();
+}
+
+function totalGeneral(){
+  let totalG = document.getElementById("totalGeneral");
+  totalG.innerHTML='$ '+sistema.montoTotalGeneral();
+}
+
+function actualOrder() {
+  document.getElementById("decreciente").checked
+    ? cargarDonacionesDecre()
+    : cargarDonacionesCre();
+  let cantTotal = document.getElementById("totalDonaciones");
+  cantTotal.innerHTML = sistema.listaDonaciones.length;
+  let promedioDonacion = document.getElementById("promedioDonacion");
+  promedioDonacion.innerHTML = sistema.promedioDonaciones();
+}
+
+function cargarEstadisticas(){
+  let donanteMasBlock = document.getElementById("donanteMasBlock");
+  let donadorRepetido = document.getElementById("donadorRepetido");
+  if (sistema.masVecesDono().indexOf(",") !== -1) {
+    donadorRepetido.textContent = "";
+    donanteMasBlock.style.display = "block";
+    donanteMasBlock.textContent = "Los donantes que más veces donaron son: ";
+    donadorRepetido.style.display = "block";
+    let textoSplit = sistema.masVecesDono().split(",");
+    for (let index = 0; index < textoSplit.length; index++) {
+      let donantes = document.createElement("li");
+      donantes.textContent = textoSplit[index];
+      donadorRepetido.appendChild(donantes);
+    }
+  } else if (sistema.masVecesDono().length > 0 && !(sistema.masVecesDono().indexOf(",") !== -1)) {
+    donadorRepetido.innerHTML='';
+    donanteMasBlock.style.display = "block";
+    donanteMasBlock.textContent = 'Donante que más veces donó: '+sistema.masVecesDono();
+  }
+}
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  let totalDonaciones = sistema.cantidadDonaciones();
+  let efectivo = parseFloat(totalDonaciones+sistema.cantidadModo('efectivo')) > 1 ? parseFloat(sistema.cantidadModo('efectivo')) : 0;
+  let transferencia = parseFloat(totalDonaciones+sistema.cantidadModo('transferencia')) > 1 ? parseFloat(sistema.cantidadModo('transferencia')) : 0;
+  let canje = parseFloat(totalDonaciones+sistema.cantidadModo('canje')) > 1 ? parseFloat(sistema.cantidadModo('canje')) : 0;
+  let mercaderia = parseFloat(totalDonaciones+sistema.cantidadModo('mercaderia')) > 1 ? parseFloat(sistema.cantidadModo('mercaderia')) : 0;
+  let cheque = parseFloat(totalDonaciones+sistema.cantidadModo('cheque')) > 1 ? parseFloat(sistema.cantidadModo('cheque')) : 0;
+  let otros = parseFloat(totalDonaciones+sistema.cantidadModo('otros')) > 1 ? parseFloat(sistema.cantidadModo('otros')) : 0;
+
+  var data = google.visualization.arrayToDataTable([
+    ["Task", "Hours per Day"],
+    ["Efectivo", efectivo],
+    ["Transferencia", transferencia],
+    ["Canje", canje],
+    ["Mercaderia", mercaderia],
+    ["Cheque", cheque],
+    ["Otros", otros],
+  ]);
+
+  var options = {
+    title: "Donaciones por modo",
+    backgroundColor: 'transparent',
+    width:350,
+    height:300,
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
+
+  chart.draw(data, options);
+}
